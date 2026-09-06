3fe53ed045048bac7988fe9d3cdb8cfb282d2c4d
wj2929 <wj2929@qq.com>
2026-09-06 10:21:27 +0800

    ci: 分离 macOS 架构产物上传

--- electron-builder.staged.cjs
+++ electron-builder.staged.cjs
@@ -1,8 +1,20 @@
 const packageJson = require('./package.json')
 const packageLocales = require('./config/package-locales.json')
 
+const packageArch = process.env.MD_VIEWER_PACKAGE_ARCH
+const macConfig = packageArch === 'x64' || packageArch === 'arm64'
+  ? {
+      ...packageJson.build.mac,
+      target: packageJson.build.mac.target.map(({ target }) => ({
+        target,
+        arch: [packageArch],
+      })),
+    }
+  : packageJson.build.mac
+
 module.exports = {
   ...packageJson.build,
+  mac: macConfig,
   asar: true,
   npmRebuild: false,
   electronLanguages: packageLocales.electronLanguages,
